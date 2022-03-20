8684fd0f4962d36eddc2f2ad22151d97449cd25b
Paco020295 <paco020293@gmail.com>
2022-03-20 17:48:47 -0300

    Add Google Drive for media images

--- cvApp/models.py
+++ cvApp/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from gdstorage.storage import GoogleDriveStorage
 
+gd_storage = GoogleDriveStorage()
+
 
 class Competence(models.Model):
     title = models.CharField(max_length=50, blank=False, null=False)
     description = models.TextField(blank=True, null=True)
-    logo = models.ImageField(upload_to='logos/', blank=False, null=False)
+    logo = models.ImageField(upload_to='logos/', blank=False, null=False, storage=gd_storage)
 
     def __str__(self):
         return self.title
